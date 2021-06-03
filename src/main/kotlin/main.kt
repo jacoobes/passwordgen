3ba1925f291a4f2ae951e9d8402cb29a8f33083e
jacoobes <jacobnguyend@gmail.com>
2021-06-02 22:44:39 -0500

    First commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,21 @@
+
+import java.lang.RuntimeException
+
+
+fun main(args: Array<String>) {
+
+
+     println("How long do you want your password to be?")
+
+    val howLong = readLine()?.toIntOrNull() ?: throw RuntimeException("Not a number")
+
+    println(passwordGen(howLong))
+}
+
+fun passwordGen( length: Int) : String {
+    var len = length
+    return generateSequence { (('a'..'z') + ('A'..'Z') + ('1'..'9')).random().takeIf { len != 0 }
+        .also { len-- }  }
+        .toList()
+        .joinToString("")
+}
